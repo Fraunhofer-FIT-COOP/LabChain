3f26dfcd76b85448c62d57f6f71151f2260a7bb6
Philipp Hossner <phossner@ukaachen.de>
2018-04-17 22:53:40 +0200

    Add code skeleton for the account component and its tests

---
+++ labchain/client.py
@@ -0,0 +1,7 @@
+class BlockchainClient:
+
+    def __init__(self, transaction_factory, network_interface, crypto_helper):
+        pass
+
+    def main(self):
+        pass
---
+++ tests/test_account.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+
+
+class ManageWalletTestCase(TestCase):
+    pass
+
+
+class CreateTransactionTestCase(TestCase):
+    pass
+
+
+class LoadBlockTestCase(TestCase):
+    pass
