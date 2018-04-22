a67ad47ac05e8c21a05e0590c7d8fc4c9b856de8
Hisham <hmr.ramzi@gmail.com>
2018-04-22 15:04:13 +0200

    Fixing variable names in Consensus.py

--- common/consensus.py
+++ common/consensus.py
@@ -7,10 +7,10 @@ class Consensus:
     def __init__(self):
         self.difficulty = 1
         self.last_recalculation_timestamp = datetime.now()
-        self.blocks_mining_rate = 60  # Threshold to be defined
+        self.time_to_mine_blocks_threshold = 60  # Threshold to be defined
         self.max_diff = 12  # Threshold to be defined
         self.blocks_threshold = 60
-        self.blocks_index = 0
+        self.blocks_counter = 0
 
     def __getitem__(self, item):
         pass
@@ -24,7 +24,7 @@ class Consensus:
     def calculate_difficulty(self, timestamp):
         #
         self.difficulty = ((((timestamp - self.last_recalculation_timestamp).total_seconds()) /
-                           self.blocks_mining_rate)).floor()
+                           self.time_to_mine_blocks_threshold)).floor()
         self.last_recalculation_timestamp = timestamp
         if self.difficulty >= self.max_diff:
             self.difficulty = self.max_diff - 1
@@ -38,7 +38,7 @@ class Consensus:
         return block_hash[:self.difficulty] == zeros_array and nonce == block.nonce
 
     def mine(self, block):
-        self.blocks_index += 1
+        self.blocks_counter += 1
         zeros_array = "0" * self.difficulty
         encapsulated_block = str(block.index) + str(block.tree_hash) + str(block.pre_hash) + str(block.creator)\
                              + str(block.nonce)
@@ -48,7 +48,7 @@ class Consensus:
             encapsulated_block = (str(block.index) + str(block.tree_hash) + str(block.pre_hash) + str(block.creator)
                                   + str(block.nonce))
             block_hash = MockCryptoHelper.hash(encapsulated_block)
-        if self.blocks_index % self.blocks_threshold == 0:
-            self.blocks_index = 0
+        if self.blocks_counter % self.blocks_threshold == 0:
+            self.blocks_counter = 0
             self.calculate_difficulty(datetime.now())
         return block.nonce
