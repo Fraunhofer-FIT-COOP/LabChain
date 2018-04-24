8f0d83cb20e9c82d4788c29f320d9626a0825070
Hisham <hmr.ramzi@gmail.com>
2018-04-24 13:52:45 +0200

    Adding 2 more test false cases for mine and validate

--- tests/test_consensus.py
+++ tests/test_consensus.py
@@ -26,6 +26,22 @@ class Tests(TestCase):
         returned_nonce = consensus.mine(block)
         self.assertEqual(nonce_true, returned_nonce)
 
+    def test_show_mine_false(self):
+        #  Values Dumped
+        nonce_true = 4
+        hash_true = "00054358392586569542654697954676849367548926706893"
+        hash_false = "03422354358392586569542654697954676849367548926706893"
+        hash_list_for_nonce_4 = [hash_false, hash_false, hash_false, hash_false, hash_true]
+        #  Consensus instance with Mock
+        consensus = Consensus()
+        consensus.difficulty = 3
+        consensus.crypto_helper.hash = MagicMock(side_effect=hash_list_for_nonce_4)
+        #  Block Instance
+        block = Block(1, datetime.now(), "")
+
+        returned_nonce = consensus.mine(block)
+        self.assertFalse(nonce_true != returned_nonce)
+
     def test_show_validate_true(self):
         #  Values Dumped
         nonce_true = 4
@@ -39,6 +55,19 @@ class Tests(TestCase):
         block.nonce = nonce_true
 
         self.assertTrue(consensus.validate(block, nonce_true))
+
+    def test_show_validate_false(self):
+        #  Values Dumped
+        nonce_false = 4
+        hash_false = "03422354358392586569542654697954676849367548926706893"
+        #  Consensus instance with Mock
+        consensus = Consensus()
+        consensus.difficulty = 3
+        consensus.crypto_helper.hash = MagicMock(return_value=hash_false)
+        #  Block Instance
+        block = Block(1, datetime.now(), "")
+
+        self.assertFalse(consensus.validate(block, nonce_false))
 
     if __name__ == '__main__':
         unittest.main()
