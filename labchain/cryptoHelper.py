8550bcb40c902e82709391d12b9c4cf4ac2ec586
sezerse <selinsezer93@gmail.com>
2018-04-27 13:34:13 +0200

    CryptoHelper skeleton

---
+++ labchain/cryptoHelper.py
@@ -0,0 +1,25 @@
+class CryptoHelper:
+
+    def __init__(self):
+        self.crypto_helper = CryptoHelper(None)
+
+    def __getitem__(self, item):
+        pass
+
+    def __setitem__(self, key, value):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def sign(self):
+        pass
+
+    def validate(self):
+        pass
+
+    def generateKeyPair(self):
+        pass
+
+    def hash(self):
+        pass
