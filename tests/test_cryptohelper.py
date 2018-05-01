90aa152cbc7b4fbd27b1b8eeed0de7112129b317
sezerse <selinsezer93@gmail.com>
2018-05-01 12:16:18 +0200

    Fix test cases hash_true and hash_false

--- tests/test_cryptohelper.py
+++ tests/test_cryptohelper.py
@@ -10,20 +10,34 @@ class Tests(TestCase):
     def test_show_hash_true(self):
         #  Values Dumped
         message = {'message': 'Hello World'}
-        hash_true = '98BE8B9818C2538FD0C476F3DDD0D6B7E3A58AD77BE5CEF38A74ECDCD9C01EF1'
+        hash_true = '98be8b9818c2538fd0c476f3ddd0d6b7e3a58ad77be5cef38a74ecdcd9c01ef1'
 
         helper = CryptoHelper.instance()
-        returned_hash = helper.hash(message)
-
+        returned_hash = helper.hash(message).hexdigest()
         self.assertEqual(hash_true, returned_hash)
 
     def test_show_hash_false(self):
-        pass
+        #  Values Dumped
+        message = {'message': 'Bye World'}
+        hash_false = '98be8b9818c2538fd0c476f3ddd0d6b7e3a58ad77be5cef38a74ecdcd9c01ef1'
+
+        helper = CryptoHelper.instance()
+        returned_hash = helper.hash(message).hexdigest()
+
+        self.assertFalse(hash_false == returned_hash)
 
     def test_show_signature_true(self):
         pass
 
     def test_show_signature_false(self):
+        pass
+
+    # Is input in json format?
+    def test_signature_input_true(self):
+        pass
+
+    # Same above.
+    def test_validate_input_true(self):
         pass
 
     if __name__ == '__main__':
