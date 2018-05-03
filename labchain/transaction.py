b1876a0a2c161d48c00775e546e676f72a5d278e
Philipp Hossner <philipp.hossner@posteo.de>
2018-05-03 20:17:18 +0200

    Add Transaction class

---
+++ labchain/transaction.py
@@ -0,0 +1,31 @@
+import json
+
+
+class Transaction:
+    """Represents a single transaction within the blockchain."""
+
+    def __init__(self, sender, receiver, payload, signature=None):
+        self.sender = sender
+        self.receiver = receiver
+        self.payload = payload
+        self.signature = signature
+
+    def get_json(self):
+        """Serialize this instance to a JSON string."""
+        return json.dumps({
+            'sender': self.sender,
+            'receiver': self.receiver,
+            'payload': self.payload,
+            'signature': self.signature,
+        })
+
+    @staticmethod
+    def from_json(json_data):
+        """Deserialize a JSON string to a Transaction instance."""
+        data_dict = json.loads(json_data)
+        return Transaction.from_dict(data_dict)
+
+    @staticmethod
+    def from_dict(data_dict):
+        """Instantiate a Transaction from a data dictionary."""
+        return Transaction(data_dict['sender'], data_dict['receiver'], data_dict['payload'], data_dict['signature'])
