0dc943d392bba24e0da2835062590f4d2ba9445e
Philipp Hossner <philipp.hossner@posteo.de>
2018-05-03 20:17:34 +0200

    Edit Block class for direct attribute access and json support

--- labchain/block.py
+++ labchain/block.py
@@ -1,35 +1,60 @@
+import json
 import time
 
+from labchain.transaction import Transaction
+
+
 class Block:
-    def __init__(self, block_number, merkel_tree_root, predecessor_hash, block_creator_id, transactions):
+    """Represents a block in the blockchain."""
+
+    def __init__(self, block_number, merkel_tree_root, predecessor_hash, block_creator_id, transactions=tuple(),
+                 nonce=None, timestamp=time.time()):
         """Constructor for Block class
 
         Args:
             transactions (List): Transactions to form the block
 
         Class Variables:
-            __block_number (int): Unique ID for block created
-            __timestamp (time): Time when block was created
-            __merkle_tree_root (hash): Hash of Transactions merkle root
-            __predecessor_hash (hash): Previous Block hash value
-            __nonce (int): Nonce matching blockchain criteria
-            __block_creator_id (String): Node ID who created the block
-            __transactions (List): Transactions in the block
+            block_number (int): Unique ID for block created
+            timestamp (time): Time when block was created
+            merkle_tree_root (hash): Hash of Transactions merkle root
+            predecessor_hash (hash): Previous Block hash value
+            nonce (int): Nonce matching blockchain criteria
+            block_creator_id (String): Node ID who created the block
+            transactions (List): Transactions in the block
 
         """
-        self.__block_number = block_number
-        self.__timestamp = time.time()
-        self.__merkle_tree_root = merkel_tree_root
-        self.__predecessor_hash = predecessor_hash
-        self.__nonce = None
-        self.__block_creator_id = block_creator_id
-        self.__transactions = transactions
+        self.block_number = block_number
+        self.timestamp = timestamp
+        self.merkle_tree_root = merkel_tree_root
+        self.predecessor_hash = predecessor_hash
+        self.nonce = nonce
+        self.block_creator_id = block_creator_id
+        self.transactions = transactions
 
     def get_json(self):
-       pass
+        """Serialize this instance to a JSON string."""
+        return json.dumps(
+            {
+                'nr': self.block_number,
+                'timestamp': self.timestamp,
+                'merkleHash': self.merkle_tree_root,
+                'predecessorBlock': self.predecessor_hash,
+                'nonce': self.nonce,
+                'creator': self.block_creator_id,
+                'transactions': [transaction.get_json() for transaction in self.transactions]
+            }
+        )
 
-    def get_predecessor_hash(self):
-       return self.__predecessor_hash
+    @staticmethod
+    def from_json(json_data):
+        """Deserialize a JSON string to a Block instance."""
+        data_dict = json.loads(json_data)
+        return Transaction.from_dict(data_dict)
 
-    def get_transactions(self):
-        pass
+    @staticmethod
+    def from_dict(data_dict):
+        """Instantiate a Block from a data dictionary."""
+        return Block(data_dict['nr'], data_dict['merkleHash'], data_dict['predecessorBlock'], data_dict['creator'],
+                     [Transaction.from_dict(transaction_dict) for transaction_dict in data_dict['transactions']],
+                     data_dict['nonce'], data_dict['timestamp'])
