a6e398ca210a988419ac2502c1ac479ceb08404e
Philipp Hossner <philipp.hossner@posteo.de>
2018-05-03 21:20:43 +0200

    Add __str__ method for Block and Transaction

--- labchain/transaction.py
+++ labchain/transaction.py
@@ -10,14 +10,18 @@ class Transaction:
         self.payload = payload
         self.signature = signature
 
-    def get_json(self):
-        """Serialize this instance to a JSON string."""
-        return json.dumps({
+    def to_dict(self):
+        """Convert own data to a dictionary."""
+        return {
             'sender': self.sender,
             'receiver': self.receiver,
             'payload': self.payload,
             'signature': self.signature,
-        })
+        }
+
+    def get_json(self):
+        """Serialize this instance to a JSON string."""
+        return json.dumps(self.to_dict())
 
     @staticmethod
     def from_json(json_data):
@@ -29,3 +33,6 @@ class Transaction:
     def from_dict(data_dict):
         """Instantiate a Transaction from a data dictionary."""
         return Transaction(data_dict['sender'], data_dict['receiver'], data_dict['payload'], data_dict['signature'])
+
+    def __str__(self):
+        return str(self.to_dict())
