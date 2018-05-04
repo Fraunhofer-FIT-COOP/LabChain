cf8a5fdd2b2e1f63c050e00f1aaf3ad773e53891
oowais <oowais@gmail.com>
2018-05-04 10:35:57 +0200

    Transaction factory

--- mock/transaction.py
+++ mock/transaction.py
@@ -3,6 +3,12 @@ class Transaction:
     def __init__(self, payload):
         self.__payload = payload
 
+    def __init__(self, sender, receiver, payload, signature):
+        self.__sender = sender
+        self.__receiver = receiver
+        self.__payload = payload
+        self.__signature = signature
+
     def get_json(self):
         return self.__payload
 
