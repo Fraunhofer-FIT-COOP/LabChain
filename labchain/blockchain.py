66516cffec90613550abd66b0d031c58eb1e50ec
Saurav Das <saurav.das37@gmail.com>
2018-05-10 09:38:12 +0200

    Fixing switch_to_branch functionality

--- labchain/blockchain.py
+++ labchain/blockchain.py
@@ -1,7 +1,7 @@
 from labchain.block import LogicalBlock
 import json
 
-CHECKPOINT_SIZE = 6
+TOLERANCE_LEVEL = 6
 
 class BlockChain:
     def __init__(self, consensus_obj, txpool_obj, node_id, crypto_helper_obj):
@@ -9,7 +9,7 @@ class BlockChain:
         self._orphan_blocks = {} # parent-hash : block
         self._current_branch_heads = []
         self._node_branch_head = None
-        self._last_checkpoint = None
+        self._furthest_branching_point = {"block" : None, "position" : float("inf")}
         self._consensus = consensus_obj
         self._txpool = txpool_obj
         self._crypto_helper = crypto_helper_obj
@@ -28,27 +28,32 @@ class BlockChain:
 
         _prev_hash = block.get_predecessor_hash()
         _curr_block_hash = block.get_computed_hash()
+        _curr_block = block
 
         if _prev_hash in self._blockchain:
-            # predecessor present in Blockchain
-            if _prev_hash in self._current_branch_heads:
-                # Does not create a new branch
-                self._current_branch_heads.remove(_prev_hash)
+            _prev_block = self._blockchain.get(_prev_hash)
+            _prev_block_pos = _prev_block.get_block_pos()
 
-            _curr_head = self._blockchain[_prev_hash]
-            block.set_block_pos(_curr_head.get_block_pos() + 1)
-            self._blockchain[_curr_block_hash] = block
+            if _prev_hash in self._current_branch_heads:
+                self._current_branch_heads.remove(_prev_hash)
+            else:
+                if _prev_block_pos < self._furthest_branching_point["position"]:
+                    self._furthest_branching_point["position"] = _prev_block_pos
+                    self._furthest_branching_point["block"] = _prev_block
+
+            _curr_block.set_block_pos(_prev_block_pos + 1)
+            self._blockchain[_curr_block_hash] = _curr_block
             self._current_branch_heads.append(_curr_block_hash)
 
-            if block.is_block_ours(self._node_id):
+            if _curr_block.is_block_ours(self._node_id):
                 self._node_branch_head = _curr_block_hash
 
             # Remove transactions covered by the block from our TxPool
-            self._txpool.remove_transactions(block.get_transactions())
+            self._txpool.remove_transactions(_curr_block.get_transactions())
 
-            # Check if block is parent to some orphans
+            # Check recursively if blocks are parent to some orphans
             _parent_hash = _curr_block_hash
-            _parent_block = block
+            _parent_block = _curr_block
             while _parent_hash in self._orphan_blocks:
                 _block = self._orphan_blocks[_parent_hash]
                 self._txpool.remove_transactions(_block.get_transactions())
@@ -60,7 +65,7 @@ class BlockChain:
 
         else:
             # Put block in orphan pool, query predecessor block
-            self._orphan_blocks[_prev_hash] = block
+            self._orphan_blocks[_prev_hash] = _curr_block
             self.request_block_from_neighbour(_prev_hash)
 
         self.switch_to_longest_branch()
@@ -78,31 +83,31 @@ class BlockChain:
         return new_block
 
     def switch_to_longest_branch(self):
-        _checkpoint = self._blockchain.get(self._last_checkpoint)
-        _max_len = 0
-        _max_head = None
-
         if len(self._current_branch_heads) > 1:
-            # Branching has occured, shit!
-            # Find the maximum sized branch
+            _check_point_pos = self._furthest_branching_point['position']
+            _check_point = self._furthest_branching_point['block']
+            _check_point_hash = _check_point.get_computed_hash()
+
+            _max_len = 0
+            _max_head = None
             for _branch_head_hash in self._current_branch_heads:
                 _head = self._blockchain.get(_branch_head_hash)
-                _path_len = _head.get_block_pos() - _checkpoint.get_block_pos()
+                _path_len = _head.get_block_pos() - _check_point_pos
                 if _path_len > _max_len:
                     _max_len = _path_len
                     _max_head = _head
 
-            if _max_len > CHECKPOINT_SIZE:
+            if _max_len > TOLERANCE_LEVEL:
                 _new_head_hash = _max_head.get_computed_hash()
 
-                # Save all block hashes between checkpoint and head in max chain
+                # Save all block hashes between furthest branch and head in max chain
                 _b_hash = _new_head_hash
                 _longest_chain = []
-                while _b_hash != self._last_checkpoint:
+                while _b_hash != _check_point_hash:
                     _longest_chain.append(_b_hash)
                     _b = self._blockchain.get(_b_hash)
                     _b_hash = _b.get_predecessor_hash()
-                _longest_chain.append(self._last_checkpoint)
+                _longest_chain.append(_check_point_hash)
 
                 # Remove all other branches
                 self._current_branch_heads.remove(_new_head_hash)
@@ -116,21 +121,9 @@ class BlockChain:
                         _b_hash = _b.get_predecessor_hash()
                         del _b
 
-                self._current_branch_heads = [_new_head_hash]
+                self._current_branch_heads = [_new_head_hash,]
                 self._node_branch_head = _new_head_hash
-        else:
-            # Branching didn't occur yet
-            _max_head = self._blockchain.get(self._current_branch_heads[0])
-            _max_len = _max_head.get_block_pos() - _checkpoint.get_block_pos()
-
-        if _max_len > CHECKPOINT_SIZE:
-            i = 0
-            _new_checkpoint = _max_head
-            while i < CHECKPOINT_SIZE:
-                _new_checkpoint_hash = _new_checkpoint.get_predecessor_hash()
-                _new_checkpoint = self._blockchain.get(_new_checkpoint_hash)
-                i += 1
-            self._last_checkpoint = _new_checkpoint_hash
+                self._furthest_branching_point = {"block" : None, "position" : float("inf")}
 
     def send_block_to_neighbour(self, requested_block_hash):
         return self._blockchain[requested_block_hash]
