6ee8c35820c7352bbcefdd24b194074bc0fa33a1
Philipp Hossner <philipp.hossner@posteo.de>
2018-05-17 21:43:57 +0200

    Add bootstrap component

---
+++ labchain/bootstrap.py
@@ -0,0 +1,34 @@
+from labchain.networking import BlockDoesNotExistException
+
+
+class Bootstrapper:
+    """Bootstrap the blockchain from other nodes."""
+
+    MAX_BLOCK_REQUEST_RETRIES = 100
+
+    def __init__(self, network_interface):
+        self.network_interface = network_interface
+
+    def do_bootstrap(self, blockchain):
+        """Initialize a blockchain object with blocks from other nodes."""
+        current_block_id = 0
+        while True:
+            try:
+                retries = 0
+                while retries < self.MAX_BLOCK_REQUEST_RETRIES:
+                    block = self.network_interface.requestBlock(current_block_id)
+                    if blockchain.add_block(block):
+                        current_block_id += 1
+                        break
+                    retries += 1
+            except BlockDoesNotExistException:
+                break
+        if current_block_id == 0:
+            self._insert_genesis_block(blockchain)
+        blockchain.switch_to_longest_branch()
+        return blockchain
+
+    @staticmethod
+    def _insert_genesis_block(blockchain):
+        genesis_block = blockchain.create_block([])
+        blockchain.add_block(genesis_block)
