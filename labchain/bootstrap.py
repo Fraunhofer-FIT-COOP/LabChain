69656e4e53b4b62a8ff3ca93dc533b4103c9493e
Daniel Schuster <daniel.schuster@ima-zlw-ifu.rwth-aachen.de>
2018-06-08 10:47:18 +0200

    removed old bootstrap function

--- labchain/bootstrap.py
+++ labchain/bootstrap.py
@@ -12,24 +12,6 @@ class Bootstrapper:
 
     def do_bootstrap(self, blockchain):
         """Initialize a blockchain object with blocks from other nodes."""
-        current_block_id = 0
-        while True:
-            try:
-                retries = 0
-                while True:
-                    block = self.network_interface.requestBlock(current_block_id)
-                    if blockchain.add_block(block):
-                        current_block_id += 1
-                        break
-                    retries += 1
-                    if retries == self.MAX_BLOCK_REQUEST_RETRIES:
-                        raise BlockchainInitFailed()
-            except BlockDoesNotExistException:
-                break
-        return blockchain
-
-    def do_bootstrap_by_hash_range(self, blockchain):
-        """Initialize a blockchain object with blocks from other nodes."""
         retries = 0
         while True:
             try:
