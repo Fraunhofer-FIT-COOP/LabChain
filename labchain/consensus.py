59d117bd76a35cc985ff9bb634c4e665382ef191
Siddharth Mehrotra <the4daspect@hotmail.com>
2018-07-06 14:01:04 +0530

    added granularity

--- labchain/consensus.py
+++ labchain/consensus.py
@@ -15,10 +15,19 @@ class Consensus:
 
         self.crypto_helper = CryptoHelper.instance()
         self.max_diff = 5  # Threshold to be defined
+        self.min_diff = 1
         self.kill_mine = 0
         self.last_mine_time_sec = time.time()
-        self.expected_mine_freq = 60
-        self.diflag = True
+        self.expected_mine_freq = 30
+        self.granular = True
+        self.granular_factor = 1
+        self.diflag = False
+        if not self.granular:
+            self.diflag = True
+        if self.diflag:
+            self.granular = False
+        if self.granular:
+            self.granular_factor = 4
 
     def __getitem__(self, item):
         pass
@@ -29,46 +38,30 @@ class Consensus:
     def __iter__(self):
         pass
 
-    def calculate_difficulty_2(self, latest_timestamp, earliest_timestamp, num_of_blocks, prev_difficulty):
+    def calculate_difficulty_with_prev(self, latest_timestamp, earliest_timestamp, num_of_blocks, prev_difficulty,
+                                       bits):
         if num_of_blocks == 0 or num_of_blocks == 1:
-            return int(self.max_diff / 2)
+            return int(self.max_diff * self.granular_factor / 2)
 
         avg_time = (float(latest_timestamp - earliest_timestamp) / num_of_blocks)
         if avg_time == 0:
-            return int(self.max_diff / 2)
-
-        ratio = log(self.expected_mine_freq / avg_time, 2)
-        partial_difficulty = ratio * prev_difficulty
-        if partial_difficulty < 1:
-            return 1
-        next = int(math.ceil(partial_difficulty))
-        prev = int(math.floor(partial_difficulty))
-        current = pow(2, partial_difficulty)
-        if pow(2, next) - current < current - pow(2, prev):
-            difficulty = next
-        else:
-            difficulty = prev
-        logging.info("difficulty ================ " + str(difficulty))
-        return difficulty
-
-    def calculate_difficulty_with_prev(self, latest_timestamp, earliest_timestamp, num_of_blocks, prev_difficulty):
-        if num_of_blocks == 0 or num_of_blocks == 1:
-            return int(self.max_diff / 2)
-
-        avg_time = (float(latest_timestamp - earliest_timestamp) / num_of_blocks)
-        if avg_time == 0:
-            return int(self.max_diff / 2)
-
+            return int(self.max_diff * self.granular_factor / 2)
+        logging.info("avg time = " + str(avg_time))
         ratio = self.expected_mine_freq / avg_time
-        prev_max_attemps = pow(2, prev_difficulty)
-        current = log(ratio, 2) + float(prev_max_attemps)
-        if current <= 0:
-            return 1
-        partial_difficulty = log(current, 2)
-        if partial_difficulty < 1:
-            return 1
-        difficulty = int(math.floor(partial_difficulty))
-        logging.info("difficulty ================ " + str(difficulty))
+        prev_max_attemps = bits * prev_difficulty
+        current = math.log(ratio, 2) + float(prev_max_attemps)
+        partial_difficulty = float(current) / bits
+
+        if partial_difficulty < self.min_diff * self.granular_factor:
+            return self.min_diff * self.granular_factor
+        elif partial_difficulty > self.max_diff * self.granular_factor:
+            return self.max_diff * self.granular_factor
+
+        if partial_difficulty <= prev_difficulty:
+            difficulty = int(math.floor(partial_difficulty))
+        else:
+            difficulty = int(math.ceil(partial_difficulty))
+        logging.info("avg time=" + str(avg_time) + " prev d=" + str(prev_difficulty) + " difficulty=" + str(difficulty))
         return difficulty
 
     def calculate_difficulty(self, latest_timestamp, earliest_timestamp, num_of_blocks):
@@ -81,12 +74,19 @@ class Consensus:
         # global difficulty should not be updated, instead return difficulty,
         # because if validate is called during mining, it would update difficulty
 
-    def validate(self, block, latest_timestamp, earliest_timestamp, num_of_blocks, prev_difficulty=-1):
+    def get_difficulty(self, latest_timestamp, earliest_timestamp, num_of_blocks, prev_difficulty):
         if self.diflag or prev_difficulty < 1:
             difficulty = self.calculate_difficulty(latest_timestamp, earliest_timestamp, num_of_blocks)
+        elif not self.granular:
+            difficulty = self.calculate_difficulty_with_prev(latest_timestamp, earliest_timestamp, num_of_blocks,
+                                                             prev_difficulty, 4)
         else:
             difficulty = self.calculate_difficulty_with_prev(latest_timestamp, earliest_timestamp, num_of_blocks,
-                                                             prev_difficulty)
+                                                             prev_difficulty, 1)
+        return difficulty
+
+    def validate(self, block, latest_timestamp, earliest_timestamp, num_of_blocks, prev_difficulty=-1):
+        difficulty = self.get_difficulty(latest_timestamp, earliest_timestamp, num_of_blocks, prev_difficulty)
 
         logging.debug('#INFO: validate Difficulty: ' + str(difficulty))
         zeros_array = "0" * difficulty
@@ -94,16 +94,13 @@ class Consensus:
             str(block.predecessor_hash), 'creator': str(block.block_creator_id), 'nonce': str(block.nonce)}
         message = json.dumps(data)
         block_hash = self.crypto_helper.hash(message)  # Assumed that hash is str
+        valid = self.equalZeros(block_hash, zeros_array, difficulty)
         logging.debug('#INFO:Consensus-> Block: ' + str(block.block_id) + ' is validated with result ' + str(
-            block_hash[:difficulty] == zeros_array) + ' with hash: ' + str(block_hash))
-        return block_hash[:difficulty] == zeros_array
+            valid) + ' with hash: ' + str(block_hash))
+        return valid
 
     def mine(self, block, latest_timestamp, earliest_timestamp, num_of_blocks, prev_difficulty=-1):
-        if self.diflag or prev_difficulty < 1:
-            difficulty = self.calculate_difficulty(latest_timestamp, earliest_timestamp, num_of_blocks)
-        else:
-            difficulty = self.calculate_difficulty_with_prev(latest_timestamp, earliest_timestamp, num_of_blocks,
-                                                             prev_difficulty)
+        difficulty = self.get_difficulty(latest_timestamp, earliest_timestamp, num_of_blocks, prev_difficulty)
 
         logging.debug('#INFO: mine Difficulty: ' + str(difficulty))
         block.difficulty = difficulty
@@ -115,7 +112,7 @@ class Consensus:
         block_hash = self.crypto_helper.hash(message)  # nonce is zero (we need to check that)
         counter = 0
         block.nonce = randint(0, sys.maxsize)
-        while block_hash[:difficulty] != zeros_array:
+        while not self.equalZeros(block_hash, zeros_array, difficulty):
             if self.kill_mine == 1:
                 self.kill_mine = 0
                 # need a boolean return to check if mine got killed
@@ -141,3 +138,20 @@ class Consensus:
     #        self.blocks_counter = 0
     #        self.recalculate = 0;
     #        self.difficulty = self.calculate_difficulty(time.time())
+
+    def equalZeros(self, block_hash, zeros_array, difficulty):
+        if not self.granular:
+            return block_hash[:difficulty] == zeros_array
+        bytes_required = int(math.ceil(float(difficulty) / 4))
+        if block_hash[:bytes_required - 1] != zeros_array[:bytes_required - 1]:
+            return False
+        hex_byte = block_hash[bytes_required - 1]
+        b = bin(int(hex_byte, 16))[2:]
+        zeros = len(b)
+        if b == '0':
+            zeros -= 1
+        zeros = 4 - zeros
+        if difficulty - (bytes_required - 1) * 4 != zeros:
+            return False
+        else:
+            return True
