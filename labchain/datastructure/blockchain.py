2a2b2d5ddb7eed313c2d935f30eb0fa981fe8708
Melanie Martini <melanie.martini@rwth-aachen.de>
2019-05-08 17:19:20 +0200

    modified get_block tests and functions to throw error

--- labchain/datastructure/blockchain.py
+++ labchain/datastructure/blockchain.py
@@ -104,7 +104,8 @@ class BlockChain:
 
         if any([range_start not in self._blockchain,
                 range_end not in self._blockchain]):
-            return None
+            raise Exception("Invalid start or end range")
+            #return None
 
         blocks_range = []
         while _b_hash != range_start:
@@ -121,6 +122,8 @@ class BlockChain:
         for _, _block in self._blockchain.items():
             if _block.block_id == block_id:
                 block_list.append(_block)
+        if not block_list:
+            raise Exception("No block found with id ".format(block_id))
         return block_list
 
     def get_block_by_hash(self, block_hash):
@@ -143,6 +146,8 @@ class BlockChain:
         _req_block = self._blockchain.get(block_hash, None)
         if _req_block:
             block_info = _req_block.get_json()
+        if not block_info:
+            raise Exception("No bock found with hash ".format(block_hash))
         return block_info
 
     def get_transaction(self, transaction_hash):
