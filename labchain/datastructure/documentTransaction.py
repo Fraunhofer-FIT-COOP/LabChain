def6275b11f6d8aa731634fb747931bf9b57a3c1
Christian van Sloun <christian.van.sloun@rwth-aachen.de>
2019-05-14 12:05:00 +0200

    Created DocumentTransaction and InitialDocumentTransaction

--- labchain/datastructure/documentTransaction.py
+++ labchain/datastructure/documentTransaction.py
@@ -1,24 +1,73 @@
+from __future__ import annotations
+
 import json
+from typing import Dict
 
 from labchain.datastructure.transaction import Transaction
 
 
 class DocumentTransaction(Transaction):
 
-    def __init__(self, sender, receiver, payload):
+    def __init__(self, sender, receiver, payload, previous_transaction: str):
         super().__init__(sender, receiver, payload)
-        pass
+        self.previous_transaction = previous_transaction
+        self.in_charge = 42  # TODO in_charge
 
     def validate_transaction(self, crypto_helper):
         """
         Passing the arguments for validation with given public key and signature.
-        :param public_key: Public key of the signer in the string format.
-        :param payload: JSON of the data to be signed.
+        :param crypto_helper: CryptoHelper object
         :param result: Receeives result of transaction validation.
         """
-        data = json.dumps({
-            'sender': self.__sender,
-            'receiver': self.__receiver,
-            'payload': self.__payload
-        })
-        return crypto_helper.validate(self.sender, data, self.signature)
+        previous_transaction: DocumentTransaction = self.get_previous_transaction()
+        owner_valid = True if previous_transaction.receiver == self.sender else False
+        return self._check_permissioned_write() \
+               and self._check_process_definition() \
+               and owner_valid and super().validate_transaction(crypto_helper)
+
+    def _check_permissioned_write(self):
+        dict: Dict = json.loads(self.payload)
+        permission = self.get_initial_transaction().permission
+        for key in dict:
+            if not self.get_previous_transaction().in_charge in permission[key]:
+                return False
+        return True
+
+    def _check_process_definition(self):
+        process_definition: Dict = self.get_initial_transaction().get_process_definition()
+        previous_transaction: DocumentTransaction = self.get_previous_transaction()
+        in_charge = previous_transaction.in_charge
+        possible_receivers = process_definition[in_charge]
+        return True if get_pid_of_receiver(
+            self.receiver) in possible_receivers else False  # TODO associate receiver-public-key to corresponding PID
+
+    """
+    Getter for the previous transaction in the workflow
+    :returns previous_transaction: The DocumentTransaction-Object of the previous transaction
+    """
+
+    def get_previous_transaction(self) -> DocumentTransaction:
+        # TODO get previous transaction
+        pass
+
+    """
+    Getter for the initial transaction in the workflow
+    :returns initial_transaction: The DocumentTransaxction-Object of the initial transaction
+    """
+
+    def get_initial_transaction(self) -> InitialDocumentTransaction:
+        # TODO get initial transaction
+        pass
+
+
+class InitialDocumentTransaction(DocumentTransaction):
+
+    def __init__(self, sender, receiver, payload, process: Dict,
+                 permission: Dict, previous_transaction=''):
+        super().__init__(sender, receiver, payload, previous_transaction)
+        self.process = process
+        self.permission = permission
+        # TODO transaction_type???
+
+    def get_process_definition(self) -> Dict:
+        pass  # TODO get process definition
