0d296b21ba810a79b1f579226891bb6ef287e79f
Christian van Sloun <christian.van.sloun@rwth-aachen.de>
2019-06-21 13:19:41 +0200

    Validation, check if workflow_id is already existing

--- labchain/datastructure/taskTransaction.py
+++ labchain/datastructure/taskTransaction.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import threading
 from base64 import b64decode
 from typing import Dict
 
@@ -10,6 +11,7 @@ from labchain.util.cryptoHelper import CryptoHelper
 
 
 class TaskTransaction(Transaction):
+    _validation_lock = threading.Lock()
 
     def __init__(self, sender, receiver, payload: Dict, signature=None):
         super().__init__(sender, receiver, payload, signature)
@@ -22,53 +24,64 @@ class TaskTransaction(Transaction):
         :param blockchain: Blockchain object
         :return result: True if transaction is valid
         """
+        TaskTransaction._validation_lock.acquire()
         if self.payload['transaction_type'] is not '2' and self.payload['transaction_type'] is not '1':
             logging.warning('Transaction has wrong transaction type')
+            TaskTransaction._validation_lock.release()
             return False
 
         previous_transaction: TaskTransaction = blockchain.get_transaction(self.previous_transaction)[0]
         workflow_transaction: WorkflowTransaction = blockchain.get_transaction(self.workflow_transaction)[0]
 
         if previous_transaction is None:
+            TaskTransaction._validation_lock.release()
             raise ValueError(
                 'Corrupted transaction, no previous_transaction found')
 
         if self.workflow_ID != previous_transaction.workflow_ID:
             logging.warning('Workflow-ID of the new transaction does not match with the previous transaction.')
+            TaskTransaction._validation_lock.release()
             return False
 
         if self.workflow_ID != workflow_transaction.workflow_ID:
             logging.warning('Workflow-ID of the new transaction does not match with the initial transaction.')
+            TaskTransaction._validation_lock.release()
             return False
 
         if not previous_transaction.receiver == self.sender:
             logging.warning(
                 'Sender is not the receiver of the previous transaction!')
+            TaskTransaction._validation_lock.release()
             return False
 
         if not previous_transaction.in_charge.split(sep='_')[0] == self.sender:
             logging.warning(
                 'Sender is not the current owner of the document flow!')
+            TaskTransaction._validation_lock.release()
             return False
 
         if not self.in_charge.split(sep='_')[0] == self.receiver:
             logging.warning('Receiver does not correspond to in_charge flag')
+            TaskTransaction._validation_lock.release()
             return False
 
         if not self._check_permissions_write(previous_transaction, workflow_transaction):
             logging.warning('Sender has not the permission to write!')
+            TaskTransaction._validation_lock.release()
             return False
 
         if not self._check_process_definition(previous_transaction, workflow_transaction):
             logging.warning(
                 'Transaction does not comply to process definition!')
+            TaskTransaction._validation_lock.release()
             return False
 
         if not self._check_for_duplicate_transactions(blockchain):
             logging.warning(
                 'Duplicated transaction found!')
+            TaskTransaction._validation_lock.release()
             return False
-
+        TaskTransaction._validation_lock.release()
         return self.validate_transaction_common(crypto_helper, blockchain)
 
     def validate_transaction_common(self, crypto_helper, blockchain):
@@ -200,24 +213,42 @@ class WorkflowTransaction(TaskTransaction):
                                    data_dict['payload'], data_dict['signature'])
 
     def validate_transaction(self, crypto_helper, blockchain):
+        TaskTransaction._validation_lock.acquire()
         if self.payload['transaction_type'] is not '1':
             logging.warning('Transaction has wrong transaction type')
+            TaskTransaction._validation_lock.release()
             return False
+
+        # Check if workflow_id is already present
+        list_of_transactions = blockchain.get_all_transactions()
+        list_of_task_transaction = [TaskTransaction.from_json(t.get_json_with_signature())
+                                    for t in list_of_transactions if 'workflow_id' in t.payload]
+        list_of_workflow_transactions = [t for t in list_of_task_transaction if t.type == '1']
+        for workflow_tuple in list_of_workflow_transactions:
+            workflow = workflow_tuple[0]
+            if self.payload['workflow_id'] == workflow.payload['workflow_id']:
+                TaskTransaction._validation_lock.release()
+                return False  # TODO write a test!
 
         for sender, receivers in self.processes.items():
             if not self._check_pid_well_formedness(sender):
+                TaskTransaction._validation_lock.release()
                 return False
             for receiver in receivers:
                 if not self._check_pid_well_formedness(receiver):
+                    TaskTransaction._validation_lock.release()
                     return False
         document_keys = self.document.keys()
         for attr, pids in self.permissions.items():
             for pid in pids:
                 if not self._check_pid_well_formedness(pid):
+                    TaskTransaction._validation_lock.release()
                     return False
             if attr not in document_keys:
+                TaskTransaction._validation_lock.release()
                 return False
 
+        TaskTransaction._validation_lock.release()
         return super().validate_transaction_common(crypto_helper, blockchain)
 
     @property
