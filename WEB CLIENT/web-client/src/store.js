2bd40bd6e64cb1ed6cadeddf89061869358e0def
imonbayazid <imon_bayazid@yahoo.com>
2019-06-24 21:36:23 +0200

    API communication,UI,component build up for WEB frontend

--- WEB CLIENT/web-client/src/store.js
+++ WEB CLIENT/web-client/src/store.js
@@ -4,7 +4,51 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    patientDiagnosisData: []
+  },
+  getters: {
+    getPatientDiagnosisData: state => state.patientDiagnosisData
+  },
+  mutations: {
+    setPatientDiagnosisData(state, data = []) {
+      state.patientDiagnosisData = data;
+    }
+  },
+  actions: {
+    createCase({commit}, payload) {
+      Vue.http
+        .post(`api/article/countPerMonth`, [
+          {
+            dr_name: payload["dr_name"] ? payload["dr_name"] : "",
+            e_physician: payload["e_physician"] ? payload["e_physician"] : "",
+            dpt_chief: payload["dpt_chief"] ? payload["dpt_chief"] : ""
+          }
+        ])
+        .promise.always(response => {
+          console.log(response);
+        });
+    },
+    createCaseDemo({commit},payload) {
+      console.log(payload)
+      Vue.http
+        .post(`https://reqres.in/api/users`, [
+          {
+            name: payload.dr_name ? payload.dr_name : "default",
+            job: payload.e_physician ? payload.e_physician : "111"
+          }
+        ])
+        .then(res => {
+          console.log(res);
+        });
+    },
+    requestPatientDiagnosisData({ commit }) {
+      Vue.http
+        .get(`http://dummy.restapiexample.com/api/v1/employees`)
+        .then(response => {
+          console.log(response);
+          commit("setPatientDiagnosisData", response.body);
+        });
+    }
+  }
 });
--- WEB CLIENT/web-client/src/views/Home.vue
+++ WEB CLIENT/web-client/src/views/Home.vue
@@ -1,18 +1,91 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col cols="2"></b-col>
+        <b-col cols="8">
+          <div>
+            <b-card no-body>
+              <b-tabs card>
+                <b-tab title="Hospital" active>
+                  <b-card-text>
+                    <p>Create Case</p>
+                    <b-container fluid>
+                      <b-row class="hospital-tbl-row">
+                        <b-col sm="4">
+                          <label :for="`type-text`">Doctor Name:</label>
+                        </b-col>
+                        <b-col sm="5">
+                          <b-form-input v-model="dr_name"></b-form-input>
+                        </b-col>
+                        <b-col cols="3"></b-col>
+                        <b-col sm="4">
+                          <label :for="`type-text`">Physician Name:</label>
+                        </b-col>
+                        <b-col sm="5">
+                          <b-form-input v-model="e_physician_name"></b-form-input>
+                        </b-col>
+                        <b-col cols="3"></b-col>
+                        <b-col sm="4">
+                          <label :for="`type-text`">Fire Dpt. Chief Name:</label>
+                        </b-col>
+                        <b-col sm="5">
+                          <b-form-input v-model="dpt_chief_name"></b-form-input>
+                        </b-col>
+                        <b-col cols="3"></b-col>
+                      </b-row>
+                      <b-button variant="success" @click="createCase()">Create</b-button>
+                    </b-container>
+                  </b-card-text>
+                </b-tab>
+                <b-tab title="Doctor">
+                  <b-card-text>Tab Contents 2</b-card-text>
+                </b-tab>
+                <b-tab title="Emergency Physician">
+                  <b-card-text>Tab Contents 2</b-card-text>
+                </b-tab>
+                <b-tab title="Fire Department">
+                  <b-card-text>Tab Contents 2</b-card-text>
+                </b-tab>
+              </b-tabs>
+            </b-card>
+          </div>
+        </b-col>
+        <b-col cols="2"></b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+//import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "home",
+  data() {
+    return {
+      dr_name: "",
+      e_physician_name: "",
+      dpt_chief_name: ""
+    };
+  },
+  mounted() {
+    this.$store.dispatch("requestPatientDiagnosisData");
+  },
+  methods: {
+    createCase() {
+		let payload={
+			dr_name:this.dr_name,
+			e_physician: this.e_physician_name,
+			dpt_chief: this.dpt_chief_name
+		}
+		console.log(payload)
+		this.$store.dispatch("createCaseDemo",payload);
+	}
+  },
   components: {
-    HelloWorld
+    //HelloWorld
   }
 };
 </script>
